8297990f46483cc2811ec85281812f9a03babc53
karl0706 <karl.dosantos@groupe-esigelec.org>
2024-11-26 16:20:00 +0100

    test commit on new branch

--- test/test_register_app.py
+++ test/test_register_app.py
@@ -34,3 +34,4 @@ def test_get_user_name_from_input_blank(monkeypatch):
 def test_get_user_name_from_input_blank(monkeypatch):
     monkeypatch.setattr('sys.stdin', io.StringIO('Karl99'))
     assert get_user_name_from_input() == 'Karl99'
+
