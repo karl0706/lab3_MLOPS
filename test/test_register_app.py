dd3db384199c1c02709c0fdc7b4bcb050902c4ac
karl0706 <karl.dosantos@groupe-esigelec.org>
2024-11-26 16:00:28 +0100

    (Fix) test get user name

--- test/test_register_app.py
+++ test/test_register_app.py
@@ -33,4 +33,4 @@ def test_get_user_name_from_input_blank(monkeypatch):
 
 def test_get_user_name_from_input_blank(monkeypatch):
     monkeypatch.setattr('sys.stdin', io.StringIO('Karl99'))
-    assert get_user_name_from_input() is None
+    assert get_user_name_from_input() == 'Karl99'
